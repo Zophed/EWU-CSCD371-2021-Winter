2032a4d506fabc8c5655fb4bdb4c6b2f0d91e50f
Zophed <74560974+Zophed@users.noreply.github.com>
2021-01-08 22:27:34 -0800

    Added a unit test for DisplayQuestion()

--- PrincessBrideTrivia/PrincessBrideTrivia.Tests/ProgramTests.cs
+++ PrincessBrideTrivia/PrincessBrideTrivia.Tests/ProgramTests.cs
@@ -1,4 +1,5 @@
 using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
 using System.IO;
 
 namespace PrincessBrideTrivia.Tests
@@ -41,6 +42,20 @@ namespace PrincessBrideTrivia.Tests
 
             // Assert
             Assert.AreEqual(expectedResult, displayResult);
+        }
+
+        [TestMethod]
+        public void DisplayQuestion_ReturnsQuestionThatExists()
+        {
+            //Arrange
+
+
+            //Act
+
+
+            //Assert
+
+
         }
 
         [TestMethod]
