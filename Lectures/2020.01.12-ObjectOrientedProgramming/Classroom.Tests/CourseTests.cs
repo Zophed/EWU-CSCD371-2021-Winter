957e20393ff648e6d6303a0d872cbd19be54d61b
Anthony Onstott <tonstott34@hotmail.com>
2021-01-14 13:50:25 -0800

    Updated my notes for this

---
+++ Lectures/2020.01.12-ObjectOrientedProgramming/Classroom.Tests/CourseTests.cs
@@ -0,0 +1,13 @@
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace Classroom.Tests
+{
+    [TestClass]
+    public class CourseTests
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+        }
+    }
+}
