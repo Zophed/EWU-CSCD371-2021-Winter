2ee1610c418b45ccb788aa4d5a8c3ef174b04d7e
Anthony Onstott <tonstott34@hotmail.com>
2021-01-18 14:28:25 -0800

    Still need to implement some unit tests, find issues in code, everything has been implemented however.

--- Logger.Tests/LogFactoryTests.cs
+++ Logger.Tests/LogFactoryTests.cs
@@ -6,6 +6,19 @@ namespace Logger.Tests
     [TestClass]
     public class LogFactoryTests
     {
+        private static readonly string filePath = "Logger.Tests";
         
+        [TestMethod]
+        public void ConfigureFileLogger_SetsFilePath()
+        {
+            //Arrange
+            var logger = new LogFactory();
+            logger.ConfigureFileLogger(filePath);
+
+            //Act
+
+            //Assert
+            Assert.AreEqual(filePath, logger.FilePath);
+        }
     }
 }
--- Logger/LogFactory.cs
+++ Logger/LogFactory.cs
@@ -1,11 +1,45 @@
-﻿namespace Logger
+﻿using System;
+using System.IO;
+
+namespace Logger
 {
     public class LogFactory
     {
+        public string? FilePath { get; private set; }
+        private string? ClassName { get; set; } = "LogFactory";
         public BaseLogger CreateLogger(string className)
         {
+            BaseLogger? logger = null;
+            if (className == nameof(FileLogger))
+            {
+                ClassName = className;
+                string filePath = GetFilePath();
+                ConfigureFileLogger(filePath);
+#pragma warning disable CS8604 // Possible null reference argument.
+                FileLogger fileLogger = new FileLogger(FilePath);
+#pragma warning restore CS8604 // Possible null reference argument.
+                logger = fileLogger;
+                
+            }
+            if(logger != null)
+            {
+                return logger;
+            }
             
-            return null;
+            else
+            {
+                throw new NullReferenceException("BaseLogger is null, try again");
+            }
+        }
+
+        public void ConfigureFileLogger(string filePath)
+        {
+            FilePath = filePath;
+        }
+
+        private static string GetFilePath()
+        {
+            return Path.GetRandomFileName();
         }
     }
 }
